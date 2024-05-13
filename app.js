fafd09d4ac22d1bec87bd2042053d3ff7e154de0
Sejal Kushwaha <sejalkushwaha768@gmail.com>
2024-05-13 19:39:52 +0100

    app.js

---
+++ app.js
@@ -0,0 +1,62 @@
+
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+for (let select of dropdowns) {
+  for (currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+
+  const URL =
+  `https://v6.exchangerate-api.com/v6/01b28d7a3b7d724a40c3604f/latest/${fromCurr.value}`;
+ fetch(URL).then(response => response.json()).then(result => {
+  let exchangeRate = result.conversion_rates[toCurr.value];
+  let totalExchangeRate = (amtVal * exchangeRate).toFixed(2);
+  const exchangeRateTxt = document.querySelector(".msg");
+  exchangeRateTxt.innerText = `${amtVal}${fromCurr.value} = ${totalExchangeRate}${toCurr.value}`;
+
+ })
+
+};
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
